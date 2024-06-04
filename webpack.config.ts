8de16b7d011d7992eb61dd2f6104720a8e9771a9
Murrr-Andrew <murrr.andrew@gmail.com>
2024-06-04 11:22:16 +0200

    ulbi: feature-sliced design

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,11 +8,13 @@ export default (env: BuildEnv) => {
     entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "build"),
     html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
   };
 
   const mode = env.mode || "development";
-  const isDev = mode === "development";
   const PORT = env.port || 3000;
+
+  const isDev = mode === "development";
 
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
