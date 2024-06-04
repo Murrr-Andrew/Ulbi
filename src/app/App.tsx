c9a5f734a8135825dba6cd02fcb4ee956b820564
Murrr-Andrew <murrr.andrew@gmail.com>
2024-06-04 19:16:29 +0200

    ulbi: adding widget and ui components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 import "./styles/index.scss";
 
 const App = () => {
@@ -11,8 +11,7 @@ const App = () => {
   return (
     <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
     </div>
   );
